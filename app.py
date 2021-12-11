7314e14b747274ef3b222fea2ece6916c6fc7872
Ника_комп <v.matuk@skyeng.ru>
2021-12-11 18:02:25 +0300

    commit_1

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import json
+
 from flask import Flask, request, render_template, send_from_directory
-# from functions import ...
+from functions import get_data, open_file, kakaya_to_funksia
 
 POST_PATH = "posts.json"
 UPLOAD_FOLDER = "uploads/images"
@@ -9,13 +11,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def page_index():
-    pass
+    tags = get_data(open_file('posts.json'))
+    return render_template("index.html", tags=tags)
 
 
 @app.route("/tag")
 def page_tag():
-    pass
-
+    tag = request.args.get("tag")
+    if tag is None:
+        return "Выберите тег"
+    data = open_file('posts.json')
+    posts = kakaya_to_funksia(data, tag)
+    return render_template('post_by_tag.html', posts=posts, tag=tag)
 
 @app.route("/post", methods=["GET", "POST"])
 def page_post_create():
@@ -24,8 +31,8 @@ def page_post_create():
 
 @app.route("/uploads/<path:path>")
 def static_dir(path):
-    return send_from_directory("uploads", path)
+    # return send_from_directory("uploads", path)
+    pass
 
-
-app.run()
+app.run(debug=True)
 
--- functions.py
+++ functions.py
@@ -1,1 +1,24 @@
 import json
+
+def open_file(file):
+    with open(file, encoding='utf-8') as f:
+        return json.load(f)
+
+
+def get_data(data):
+    result = set()
+
+    for record in data:
+        content = record['content']
+        words = content.split()
+        for word in words:
+            if word.startswith('#'):
+                result.add(word[1:])
+    return result
+
+def kakaya_to_funksia(data, tag):
+    results = []
+    for record in data:
+        if f'#{tag}' in record['content']:
+            results.append(record)
+    return results
