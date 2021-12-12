442c05e91548d649bf9697093cae904c914776bf
Ника_комп <v.matuk@skyeng.ru>
2021-12-12 18:14:18 +0300

    commit_1

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import json
 
-from flask import Flask, request, render_template, send_from_directory
-from functions import get_data, open_file, kakaya_to_funksia
+from flask import Flask, request, render_template, send_from_directory, abort
+from functions import get_data, open_file, kakaya_to_funksia, add_post
 
 POST_PATH = "posts.json"
 UPLOAD_FOLDER = "uploads/images"
@@ -18,21 +18,36 @@ def page_index():
 @app.route("/tag")
 def page_tag():
     tag = request.args.get("tag")
-    if tag is None:
-        return "Выберите тег"
+    if not tag:
+        abort(400)
     data = open_file('posts.json')
     posts = kakaya_to_funksia(data, tag)
     return render_template('post_by_tag.html', posts=posts, tag=tag)
 
 @app.route("/post", methods=["GET", "POST"])
 def page_post_create():
-    pass
+    if request.method == 'GET':
+        return render_template('post_form.html')
+
+    content = request.form.get('content')
+    picture = request.files.get('picture')
+    if not content or not picture:
+        abort(400)
+    path = f'{UPLOAD_FOLDER}/{picture.filename}'
+    post = {
+        'content': content,
+        'pic' : f'/{path}'
+    }
+
+    picture.save(path)
+    add_post(POST_PATH, post)
+    return render_template('post_uploaded.html', post=post)
 
 
 @app.route("/uploads/<path:path>")
 def static_dir(path):
-    # return send_from_directory("uploads", path)
-    pass
+    return send_from_directory("uploads", path)
+
 
 app.run(debug=True)
 
--- functions.py
+++ functions.py
@@ -1,13 +1,13 @@
 import json
 
-def open_file(file):
-    with open(file, encoding='utf-8') as f:
+def open_file(filename):
+    with open(filename, encoding='utf-8') as f:
         return json.load(f)
 
 
 def get_data(data):
+    """Получаем список слов, в начале которых стоит #"""
     result = set()
-
     for record in data:
         content = record['content']
         words = content.split()
@@ -16,9 +16,17 @@ def get_data(data):
                 result.add(word[1:])
     return result
 
+
 def kakaya_to_funksia(data, tag):
+    """Получаем список из словарей, в контенте которых есть хештег {tag}"""
     results = []
     for record in data:
         if f'#{tag}' in record['content']:
             results.append(record)
     return results
+
+def add_post(post, filename):
+    data = open_file(filename)
+    data.append(post)
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4, sort_keys=True)
